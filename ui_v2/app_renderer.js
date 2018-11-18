a707d2304b8ab7d3974b00a8fae851a5a50ee653
Tomáš Tomovčík <tomovcik.tomas@gmail.com>
2018-11-18 15:51:13 +0100

    Nové UI (Preview), reset aplikácie

---
+++ ui_v2/app_renderer.js
@@ -0,0 +1,21 @@
+function startTime() {
+    var today = new Date();
+    var day = today.getDate();
+    var month = today.getMonth();
+    var year = today.getFullYear();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('date_time').innerHTML =
+        day + "." + month + "." + year + " " + h + ":" + m + ":" + s; // DD.MM.YYYY HH:MM
+    var t = setTimeout(startTime, 500);
+}
+
+function checkTime(i) {
+    if (i < 10) {
+        i = "0" + i
+    };
+    return i;
+}
