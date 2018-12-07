61aec843233604306d185f3849ebeccd75bc9eb0
Tomáš Tomovčík <tomovcik.tomas@gmail.com>
2018-12-07 15:04:36 +0100

    Pridaný setTimeout pre prepareDashboard()

--- app/js/renderer_settings.js
+++ app/js/renderer_settings.js
@@ -25,52 +25,54 @@ var storedValue_toggleAutoTheming = store.get('toggleAutoTheming');
 var storedValue_applicationPassword = store.get('applicationPassowrd');
 var settingsForms = [schoolName, edupageServerAddress, additionalWebPages]
 
-if (settingsForms.indexOf(settingsForms.length) == 'undefined') {
-    $(schoolName).attr('placeholder', store.get('schoolName'));
-    $(edupageServerAddress).attr('placeholder', store.get('edupageServerAddress'));
-    $(additionalWebPages).attr('placeholder', store.get('additionalWebPages'));
+setTimeout(function prepareDashboard() {
+    if (settingsForms.indexOf(settingsForms.length) == 'undefined') {
+        $(schoolName).attr('placeholder', store.get('schoolName'));
+        $(edupageServerAddress).attr('placeholder', store.get('edupageServerAddress'));
+        $(additionalWebPages).attr('placeholder', store.get('additionalWebPages'));
 
-} else {
-    $(schoolName).attr('placeholder', 'Nazov školy');
-    $(edupageServerAddress).attr('placeholder', 'https://www.skola.edupage.org');
-    $(additionalWebPages).attr('placeholder', 'https://www.4chan.org, https://www.ayy-lmao.com');
-}
+    } else {
+        $(schoolName).attr('placeholder', 'Nazov školy');
+        $(edupageServerAddress).attr('placeholder', 'https://www.skola.edupage.org');
+        $(additionalWebPages).attr('placeholder', 'https://www.4chan.org, https://www.ayy-lmao.com');
+    }
 
-if (storedValue_schoolName == '*' || storedValue_schoolName == ' ') {
-    overviewCard_schoolName.innerHTML = 'Názov školy nie je nastavený'
-    $(loadSpinner_card_schoolName).remove();
-} else {
-    overviewCard_schoolName.innerHTML = storedValue_schoolName;
-    $(loadSpinner_card_schoolName).remove();
-}
-if (storedValue_edupageServer == '*' || storedValue_edupageServer == ' ') {
-    overviewCard_edupageServer.innerHTML = 'Edupage server nie je nastavený'
-    $(loadSpinner_card_edupageServerAddress).remove();
-} else {
-    overviewCard_edupageServer.innerHTML = storedValue_edupageServer;
-    $(loadSpinner_card_edupageServerAddress).remove();
-}
-if (storedValue_additionalWebPages == '*' || storedValue_additionalWebPages ==' ') {
-    overviewCard_additionalPages.innerHTML = 'Dodatočné stránky nie sú nastavené';
-    $(loadSpinner_card_additionalPages).remove();
-} else {
-    overviewCard_additionalPages.innerHTML = 'Dodatočné stránky sú nastavené'
-    $(loadSpinner_card_additionalPages).remove();
-}
-if (storedValue_toggleAutoTheming == 'enabled' || storedValue_toggleAutoTheming !==' ') {
-    overviewCard_autoTheming.innerHTML = 'Automatická zmena témy je povolená';
-    $(loadSpinner_card_autoTheming).remove();
-} else {
-    overviewCard_autoTheming.innerHTML = 'Automatická zmena témy je zakázaná';
-    $(loadSpinner_card_autoTheming).remove();
-}
-if (storedValue_applicationPassword == '*' || storedValue_applicationPassword ==' ') {
-    overviewCard_passwordState.innerHTML = 'Heslo je nastavené';
-    $(loadSpinner_card_password).remove();
-} else {
-    overviewCard_passwordState.innerHTML = 'Heslo nie je nastavené'
-    $(loadSpinner_card_password).remove();
-}
+    if (storedValue_schoolName == '*' || storedValue_schoolName == ' ') {
+        overviewCard_schoolName.innerHTML = 'Názov školy nie je nastavený'
+        $(loadSpinner_card_schoolName).remove();
+    } else {
+        overviewCard_schoolName.innerHTML = storedValue_schoolName;
+        $(loadSpinner_card_schoolName).remove();
+    }
+    if (storedValue_edupageServer == '*' || storedValue_edupageServer == ' ') {
+        overviewCard_edupageServer.innerHTML = 'Edupage server nie je nastavený'
+        $(loadSpinner_card_edupageServerAddress).remove();
+    } else {
+        overviewCard_edupageServer.innerHTML = storedValue_edupageServer;
+        $(loadSpinner_card_edupageServerAddress).remove();
+    }
+    if (storedValue_additionalWebPages == '*' || storedValue_additionalWebPages == ' ') {
+        overviewCard_additionalPages.innerHTML = 'Dodatočné stránky nie sú nastavené';
+        $(loadSpinner_card_additionalPages).remove();
+    } else {
+        overviewCard_additionalPages.innerHTML = 'Dodatočné stránky sú nastavené'
+        $(loadSpinner_card_additionalPages).remove();
+    }
+    if (storedValue_toggleAutoTheming == 'enabled' || storedValue_toggleAutoTheming !== ' ') {
+        overviewCard_autoTheming.innerHTML = 'Automatická zmena témy je povolená';
+        $(loadSpinner_card_autoTheming).remove();
+    } else {
+        overviewCard_autoTheming.innerHTML = 'Automatická zmena témy je zakázaná';
+        $(loadSpinner_card_autoTheming).remove();
+    }
+    if (storedValue_applicationPassword == '*' || storedValue_applicationPassword == ' ') {
+        overviewCard_passwordState.innerHTML = 'Heslo je nastavené';
+        $(loadSpinner_card_password).remove();
+    } else {
+        overviewCard_passwordState.innerHTML = 'Heslo nie je nastavené'
+        $(loadSpinner_card_password).remove();
+    }
+}, 2000);
 
 
 function saveSettings() {
